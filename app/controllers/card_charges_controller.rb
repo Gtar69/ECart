84fd2204c254cbe6993a10a16523430737421bba
Gtar <chihweichang69@gmail.com>
2014-06-29 14:51:23 +0800

    stripe service feature modification

--- app/controllers/card_charges_controller.rb
+++ app/controllers/card_charges_controller.rb
@@ -3,35 +3,23 @@ class CardChargesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # 用Service Object
-    #CardChargeService.new()
+    
     @order = current_user.orders.find_by_token(params[:order_id])
     @amount = @order.total * 100 # in cents
    
-    Stripe.api_key = Settings.stripe_secret_key
-
-    customer = Stripe::Customer.create(
-      :email => current_user.email,
-      :card  => params[:stripeToken]
-      )
-
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => @order.token ,
-      :currency    => 'usd'
+    charge = StripeCharge.create(
+      :amount => @amount,
+      :card => params[:stripeToken],
+      :description => @order.token
     )
-
-    @order.set_payment_with!("credit_card")
-    @order.make_payment! 
-    # 信⽤用卡付款成功也要寄信通知
-    OrderMailer.notify_order_placed(@order).deliver
-    redirect_to order_path(@order.token), :notice => "成功完成付款"
-
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
+    if charge.successful?
+      @order.set_payment_with!("credit_card")
+      @order.make_payment!
+      redirect_to order_path(@order.token), :notice => "成功完成付款"
+    else    
+      flash[:error] = charge.error_message
       render "orders/pay_with_credit_card"
+    end
   end
 
 end
--- app/services/order_placing_service.rb
+++ app/services/order_placing_service.rb
@@ -5,9 +5,11 @@ class OrderPlacingService
   end
  
   def place_order!
+    # 建立訂單過程
     @order.build_item_cache_from_cart(@cart)
     @order.calculate_total!(@cart)
-    # 創建完Order後 後台必須能寄一封信給客戶
+    # using destroy! intead of clear! becasue cart is not a collection  
+    @cart.destroy!
     # 另外OrderMailer 是Class為何可以直接用method instead instantiate an object
     OrderMailer.notify_order_placed(@order).deliver
   end
